0eead47317a6f95d3600d44829925738689f4a1b
muamarzidan <codeofomiru11@gmail.com>
2024-02-10 15:22:31 +0700

    feat: add day 2

--- Javascript/Fundamental/2Multiply.js
+++ Javascript/Fundamental/2Multiply.js
@@ -1,8 +1,43 @@
+//Question : This code does not execute properly. Try to figure out why.
+// function multiply(a, b){
+//     a * b
+// }
+// Sortby : Popularity
+// Type : Fundamental
+// Difficulty: 8 kyu / easiest
+
+
+// ANSWER
+
+// simple code
+function multiply(a, b){
+    return a * b
+}
+
+// compleks code
 const multiply = (a, b) => {
     if (typeof a === "number" && typeof b === "number") {
         return a * b;
     } else {
-        return "Please input numbers"
+        return "Please input numbers";
     }
 }
-multiply(2, 2); // 4
+
+// more complex code
+const multiply = (a, b) => {
+    if (typeof a === "number" && typeof b === "number") {
+        return a * b;
+    } else if (typeof a !== "number" && typeof b === "number") {
+        return "Please input a number for the first parameter";
+    } else if (typeof a !== "number" && typeof b !== "number") {
+        return "Please input a number for the second parameter";
+    } else {
+        return "Please input numbers";
+    }
+}
+
+// simple and clean code
+const multiply = (a, b) => a * b;
+
+
+
