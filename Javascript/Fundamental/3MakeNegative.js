4fa36e594a88e803aa11df8a45c3ab33fe21a055
muamarzidan <codeofomiru11@gmail.com>
2024-02-12 11:07:51 +0700

    add day 3

---
+++ Javascript/Fundamental/3MakeNegative.js
@@ -0,0 +1,50 @@
+//Question : In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+// Examples
+// makeNegative(1);
+// makeNegative(-5);  
+// makeNegative(0);  
+// makeNegative(0.12);
+
+// Notes
+// The number can be negative already, in which case no change is required.
+// Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+
+// ANSWER
+
+//bar bar code
+// function makeNegative(num) {
+//   if (num > 0) {
+//       return -num;
+//   } else {
+//       return num;
+//   }
+// }
+
+//simple code
+// function makeNegative(num) {
+//   return num < 0 ? num : -num;
+// }
+
+// clean code
+// function makeNegative(num) {
+//   return -Math.abs(num);
+// }
+
+// more clean code
+// const makeNegative = (num) => -Math.abs(num);
+
+// more complex code
+function makeNegative(num) {
+  if (typeof num !== 'number') {
+      return 'Inputan / Assigment is not a number';
+  } else if (num === 0) {
+      return 0;
+  } else if (num > 0) {
+      return -Math.abs(num);
+  } else if (num < 0) {
+      return num;
+  } else {
+      'Nothing there'
+  }
+}
