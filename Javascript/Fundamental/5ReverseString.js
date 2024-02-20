574a3e4a3841aecfa1a52bdd0a13524ede4b748b
muamarzidan <codeofomiru11@gmail.com>
2024-02-20 23:03:19 +0700

    add day 5

---
+++ Javascript/Fundamental/5ReverseString.js
@@ -0,0 +1,36 @@
+//Question : Complete the solution so that it reverses the string passed into it.
+
+// Example 'world'  =>  'dlrow' , 'word'   =>  'drow'
+
+
+// ANSWER
+
+// bar-bar code
+function solution(str){
+    let hasilBalik = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        hasilBalik = hasilBalik + str[i];
+    };
+    return hasilBalik;
+};
+
+// another bar-bar code
+function solution(str) {
+    const hasilBalik = [...str].reverse().join("");
+    return hasilBalik;
+}
+
+// clean code
+function solution(str) {
+    const hasilBalik = str.split("").reverse().join("");
+    return hasilBalik;
+}
+
+// more clean code
+const solution = str => str.split("").reverse().join("");
+
+
+
+
+
+
